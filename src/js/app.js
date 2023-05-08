3df448630e8c9cfcb1a81755e7146f02ccb29d88
pavelklm <pavel.klm.verstka@gmail.com>
2023-05-08 21:29:21 +0300

    hihi

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+
+const api = 'cfb2790933804337b0fd3a0bf02351b4';
+
+    const url = `https://api.spoonacular.com/recipes/716429/information?apiKey=${api}&includeNutrition=true`
+    fetch(url)
+        .then((response) => {
+            response.json()
+            console.log(response);
+        })
