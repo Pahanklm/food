ecc52e6193da9157e7a39d56bdf09c59570bf34b
pavelklm <pavel.klm.verstka@gmail.com>
2023-05-15 22:34:22 +0300

    hihi

--- src/js/app.js
+++ src/js/app.js
@@ -4,75 +4,93 @@ flsFunctions.isWebp();
 
 
 const api = 'cfb2790933804337b0fd3a0bf02351b4';
-const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${api}&ingredients=sausages,cucumbers,peppers&number=2`;
+const form = document.querySelector('.search__form');
+const input = document.querySelector('.search__input');
 const header = document.querySelector('.header');
 
+form.addEventListener('submit', handleSubmit);
+
 function showCard(data, header) {
-    let html = '';
-    
-    for (let i = 0; i < data.length; i++) {
-        let title = data[i].title;
-        let image = data[i].image;
-        let usefullIngredients = data[i].usedIngredients;
-        for(let j = 0; j < usefullIngredients.length; j++){
-            let name = usefullIngredients[j].name
-            console.log(name);
-        }
+const cardsHTML = data.map((item) => {
+const title = item.title;
+const image = item.image;
+const usefulIngredients = item.usedIngredients
+    .map((ingredient) => {
+    return `<li class="card__item">${ingredient.name}</li>`;
+    })
+    .join('');
+const uselessIngredients = item.unusedIngredients
+    .map((ingredient) => {
+    return `<li class="card__item">${ingredient.name}</li>`;
+    })
+    .join('');
+const needToBuy = item.missedIngredients
+    .map((ingredient) => {
+    return `<li class="card__item">${ingredient.name}</li>`;
+    })
+    .join('');
+return `<div class="card">
+    <div class="card__main">
+        <h1 class="card__title">${title}</h1>
+        <img src="${image}" alt="img" class="card__img">
+    </div>
+    <div class="card__ingredients">
+        <div class="card__ingredients-use">
+            <h3 class="card__ingredients-title">Полезные ингредиенты</h3>
+            <ol class="card__list">
+                ${usefulIngredients}
+            </ol>
+        </div>
+        <div class="card__ingredients-useless">
+            <h3 class="card__ingredients-title">Бесполезные ингредиенты</h3>
+            <ol class="card__list">
+                ${uselessIngredients}
+            </ol>
+        </div>
+    </div>
+    <div class="card__needtobuy">
+        <h3 class="card__ingredients-title">Необходимые ингредиенты для покупки</h3>
+        <ol class="card__list">
+            ${needToBuy}
+        </ol>
+    </div>
+</div>`;
+});
 
-        html += `<div class="card">
-            <div class="card__main">
-                <h1 class="card__title">${title}</h1>
-                <img src="${image}" alt="img" class="card__img">
-            </div>
-            <div class="card__ingredients">
-                <div class="card__ingredients-use">
-                    <h3 class="card__ingredients-title">useful ingredients</h3>
-                    <ol class="card__list">
-                        <li class="card__item">1</li>
-                    </ol>
-                </div>
-                <div class="card__ingredients-useless">
-                    <h3 class="card__ingredients-title">useless ingredients</h3>
-                    <ol class="card__list">
-                        <li class="card__item">1</li>
-                    </ol>
-                </div>
-            </div>
-            <div class="card__needtobuy">
-                <h3 class="card__ingredients-title">ingredients need to buy</h3>
-                <ol class="card__list">
-                    <li class="card__item">1</li>
-                </ol>
-            </div>
-        </div>`;
-    }
-
-    header.insertAdjacentHTML('afterend', html);
+const html = cardsHTML.join('');
+header.insertAdjacentHTML('afterend', html);
 }
 
 function removeCard() {
-    const prevCard = document.querySelector('.card');
-    if (prevCard) prevCard.remove();
+const prevCard = document.querySelector('.card');
+if (prevCard) prevCard.remove();
 }
 
 function showError(header) {
-    if (header) {
-        let html = `<div class="card">Вы указали продукты, которых не существует</div>`;
-        header.insertAdjacentHTML('afterend', html);
-    }
+if (header) {
+let html = `<div class="card">Вы указали продукты, которых не существует</div>`;
+header.insertAdjacentHTML('afterend', html);
+}
 }
 
+function handleSubmit(event) {
+event.preventDefault();
+
+const ingredients = input.value.split(' ').map((ingredient) => ingredient.trim());
+const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${api}&ingredients=${ingredients.join(',+')}&number=2`;
+console.log(url);
 fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-        if (!data || data.length === 0) {
-            removeCard();
-            showError(header);
-        } else {
-            removeCard();
-            showCard(data, header);
-        }
-    });
+.then((response) => response.json())
+.then((data) => {
+    removeCard();
+
+    if (!data || data.length === 0) {
+    showError(header);
+    } else {
+    showCard(data, header);
+    }
+})
+.catch((error) => {
+    console.error('Error:', error);
+});
+}
